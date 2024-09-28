3c73167056542bc9635ea0d82c2982a906c7a767
MohinurHim <himmatovamohinur@gmail.com>
2024-09-28 11:16:21 +0300

    Add files via upload
    Namespace. Call counter

---
+++ module_3_1.py
@@ -0,0 +1,25 @@
+# Счетчик вызовов
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+def string_info(string):
+        word = string
+        result = (len(word), word.upper(), word.lower())
+        count_calls()
+        return result
+def is_contains(string, list_to_search):
+        string = string
+        list_to_search = list_to_search
+        count_calls()
+        result = False
+        for element in list_to_search:
+            if  element.lower() == string.lower:
+                result = True
+                break
+        return result
+print(string_info('World'))
+print(string_info('House'))
+print(is_contains('Glass', ['glASS', 'wINe', 'WAter']))
+print(is_contains('book', ['sheet', 'pade']))
+print(calls)
